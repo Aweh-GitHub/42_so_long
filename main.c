7aadbe635a6e13611882b5fa20e09162c12db47f
Aweh-GitHub <105616025+Aweh-GitHub@users.noreply.github.com>
2025-12-14 12:02:30 +0100

    rename mlx

--- main.c
+++ main.c
@@ -6,12 +6,18 @@
 /*   By: thantoni <thantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 19:35:15 by thantoni          #+#    #+#             */
-/*   Updated: 2025/12/13 19:37:50 by thantoni         ###   ########.fr       */
+/*   Updated: 2025/12/14 12:02:02 by thantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libmlx/mlx.h"
+#include "libft.h"
+
 int	main(int argc, char **argv)
 {
-	
+	void	*mlx_ptr;
+
+	mlx_ptr = mlx_init();
+	if (mlx_ptr == NULL)
 	return (0);
 }
