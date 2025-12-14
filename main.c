56479fd2e594ddcf6db59e76990627777f9ce7a3
Theotim Antoni <thantoni@f2r10s15.clusters.42paris.fr>
2025-12-14 17:00:11 +0100

    working static window

--- main.c
+++ main.c
@@ -6,17 +6,20 @@
 /*   By: thantoni <thantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 19:35:15 by thantoni          #+#    #+#             */
-/*   Updated: 2025/12/14 16:31:04 by thantoni         ###   ########.fr       */
+/*   Updated: 2025/12/14 16:59:47 by thantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libmlx/mlx.h"
 #include "libft/libft.h"
+#include "libmlx/mlx_int.h"
 
 int	main(int argc, char **argv)
 {
 	void	*mlx;
 	void	*window;
+	t_img	*player_img;
+	int	w, h;
 
 	(void) argc;
 	(void) argv;
@@ -24,11 +27,7 @@ int	main(int argc, char **argv)
 	if (mlx == NULL)
 		return (1);
 	window = mlx_new_window(mlx, 640, 480, "So Long");
-	void *player_img;
-	int w;
-	int h;
-
-	player_img = mlx_xpm_file_to_image(mlx, "textures/gengar_walk.png", &w, &h);
+	player_img = mlx_xpm_file_to_image(mlx, "textures/gengar_walk.xpm", &w, &h);
 	if (player_img == NULL)
 	{
 		ft_printf("impossible de charger l'image\n");
