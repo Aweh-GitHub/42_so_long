7e309eb25583e98982f1fcb0a168b4241da00f60
Theotim Antoni <thantoni@f2r10s15.clusters.42paris.fr>
2025-12-14 16:31:16 +0100

    textures en xpm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: thantoni <thantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 19:35:15 by thantoni          #+#    #+#             */
-/*   Updated: 2025/12/14 12:16:51 by thantoni         ###   ########.fr       */
+/*   Updated: 2025/12/14 16:31:04 by thantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,15 @@ int	main(int argc, char **argv)
 		return (1);
 	window = mlx_new_window(mlx, 640, 480, "So Long");
 	void *player_img;
-	int w; // Va stocker la largeur du sprite
-	int h; // Va stocker la hauteur du sprite
+	int w;
+	int h;
 
-	player_img = mlx_xpm_file_to_image(mlx, "textures/gengar.png", &w, &h);
+	player_img = mlx_xpm_file_to_image(mlx, "textures/gengar_walk.png", &w, &h);
+	if (player_img == NULL)
+	{
+		ft_printf("impossible de charger l'image\n");
+		return (1);
+	}
 	mlx_put_image_to_window(mlx, window, player_img, 100, 200);
 	mlx_loop(mlx);
 	return (0);
